4825ebd4b06e3286eda829ee1ccadd206fd33c7a
Jed Schmidt <tr@nslator.jp>
2011-02-26 21:47:08 +0900

    changed default keys to .json, made them optional

--- scripts/install.js
+++ scripts/install.js
@@ -1,5 +1,5 @@
-require( "fs" ).writeFileSync( "./lib/defaultKeys.js",
-  "module.exports = " + JSON.stringify([
+require( "fs" ).writeFileSync( "./lib/defaultKeys.json",
+  JSON.stringify([
     Array( 33 ).join( "x" ).replace( /x/g, function() {
       return ( Math.random()*16|0 ).toString(16)
     })
