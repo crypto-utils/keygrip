4268612dcd9cd1a44f78334c219fd7db0a6055b7
Jed Schmidt <tr@nslator.jp>
2012-01-15 10:17:25 +0900

    move defaultKeys

--- install.js
+++ install.js
@@ -1,4 +1,4 @@
-require( "fs" ).writeFileSync( "./lib/defaultKeys.json",
+require( "fs" ).writeFileSync( "./defaultKeys.json",
   JSON.stringify([
     Array( 33 ).join( "x" ).replace( /x/g, function() {
       return ( Math.random()*16|0 ).toString(16)
