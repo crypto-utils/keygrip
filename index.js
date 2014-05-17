a7740c78c1556b5ce7f27a7199633ed2c9c934fa
Jonathan Ong <jonathanrichardong@gmail.com>
2014-05-17 15:41:32 -0700

    Create prototype getters for defaults and validation

--- index.js
+++ index.js
@@ -9,8 +9,42 @@ function Keygrip(keys, algorithm, encoding) {
   if (!(this instanceof Keygrip)) return new Keygrip(keys, algorithm, encoding)
 
   this.keys = keys
-  this.algorithm = algorithm || 'sha1'
-  this.encoding = encoding || 'base64'
+  if (algorithm) this.algorithm = algorithm
+  if (encoding) this.encoding = encoding
+}
+
+/**
+ * Allow setting `keygrip.algorithm = 'sha1'`
+ * or `keygrip.cipher = 'aes256'`
+ * with validation instead of always doing `keygrip([], alg, enc)`.
+ * This also allows for easier defaults.
+ */
+
+Keygrip.prototype = {
+  get algorithm() {
+    return this._algorithm
+  },
+
+  set algorithm(val) {
+    if (!~crypto.getHashes().indexOf(val))
+      throw new Error('unsupported hash algorithm: ' + val)
+    this._algorithm = val
+  },
+
+  get cipher() {
+    return this._cipher
+  },
+
+  set cipher(val) {
+    if (!~crypto.getCiphers().indexOf(val))
+      throw new Error('unsupported cipher: ' + val)
+    this._cipher = val
+  },
+
+  // defaults
+  _algorithm: 'sha1',
+  _cipher: 'aes256',
+  encoding: 'base64',
 }
 
 Keygrip.prototype.sign = function Keygrip$_sign(data, key) {
