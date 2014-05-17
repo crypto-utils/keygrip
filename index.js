1043de984431355981798296e894cd9f6431dc3a
Jonathan Ong <jonathanrichardong@gmail.com>
2014-05-17 15:21:11 -0700

    use scmp lib for constant time compare

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+
 var crypto = require("crypto")
-  
+var constantTimeCompare = require('scmp')
+
+module.exports = Keygrip
+
 function Keygrip(keys, algorithm, encoding) {
   if (!algorithm) algorithm = "sha1";
   if (!encoding) encoding = "base64";
@@ -36,28 +40,3 @@ function Keygrip(keys, algorithm, encoding) {
 Keygrip.sign = Keygrip.verify = Keygrip.index = function() {
   throw new Error("Usage: require('keygrip')(<array-of-keys>)")
 }
-
-//http://codahale.com/a-lesson-in-timing-attacks/
-var constantTimeCompare = function(val1, val2){
-    if(val1 == null && val2 != null){
-        return false;
-    } else if(val2 == null && val1 != null){
-        return false;
-    } else if(val1 == null && val2 == null){
-        return true;
-    }
-
-    if(val1.length !== val2.length){
-        return false;
-    }
-
-    var matches = 1;
-
-    for(var i = 0; i < val1.length; i++){
-        matches &= (val1.charAt(i) === val2.charAt(i) ? 1 : 0); //Don't short circuit
-    }
-
-    return matches === 1;
-};
-
-module.exports = Keygrip
