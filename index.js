e8637c137f6a959dc3a6a4d68f134e416e340b64
Jonathan Ong <jonathanrichardong@gmail.com>
2014-05-17 15:30:27 -0700

    Refactor into constructor/prototype

--- index.js
+++ index.js
@@ -5,36 +5,47 @@ var constantTimeCompare = require('scmp')
 module.exports = Keygrip
 
 function Keygrip(keys, algorithm, encoding) {
-  if (!algorithm) algorithm = "sha1";
-  if (!encoding) encoding = "base64";
+  if (!Array.isArray(keys) || !keys.length) throw new Error("Keys must be provided.")
   if (!(this instanceof Keygrip)) return new Keygrip(keys, algorithm, encoding)
 
-  if (!keys || !(0 in keys)) {
-    throw new Error("Keys must be provided.")
+  this.keys = keys
+  this.algorithm = algorithm || 'sha1'
+  this.encoding = encoding || 'base64'
+}
+
+Keygrip.prototype.sign = function Keygrip$_sign(data, key) {
+  // default to the first key
+  key = key || this.keys[0]
+
+  return crypto
+    .createHmac(this.algorithm, key)
+    .update(data)
+    .digest(this.encoding)
+    .replace(/\/|\+|=/g, _sign_replace)
+}
+
+// replace base64 characters with more friendly ones
+var _sign_replacements = {
+  "/": "_",
+  "+": "-",
+  "=": ""
+}
+
+function _sign_replace(x) {
+  return _sign_replacements[x]
+}
+
+Keygrip.prototype.verify = function Keygrip$_verify(data, digest) {
+  return this.index(data, digest) > -1
+}
+
+Keygrip.prototype.index = function Keygrip$_index(data, digest) {
+  var keys = this.keys
+  for (var i = 0, l = keys.length; i < l; i++) {
+    if (constantTimeCompare(digest, this.sign(data, keys[i]))) return i
   }
 
-  function sign(data, key) {
-    return crypto
-      .createHmac(algorithm, key)
-      .update(data).digest(encoding)
-      .replace(/\/|\+|=/g, function(x) {
-        return ({ "/": "_", "+": "-", "=": "" })[x]
-      })
-  }
-
-  this.sign = function(data){ return sign(data, keys[0]) }
-
-  this.verify = function(data, digest) {
-    return this.index(data, digest) > -1
-  }
-
-  this.index = function(data, digest) {
-    for (var i = 0, l = keys.length; i < l; i++) {
-      if (constantTimeCompare(digest, sign(data, keys[i]))) return i
-    }
-
-    return -1
-  }
+  return -1
 }
 
 Keygrip.sign = Keygrip.verify = Keygrip.index = function() {
